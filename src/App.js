fd597f3f0ebb8d702060328311d825c92f7effe6
joasaavedra <joasaavedra46@gmail.com>
2023-09-18 00:09:52 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
 
+const apiURL = 'http://www.omdbapi.com/?apikey=ceffc003&s=batman'
+
 function App() {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => { 
+    console.log('cargando pelicula')
+    fetch(apiURL)
+      .then(res => res.json())
+      .then(response => {
+        const data = response.Search
+        const movies = data.map(movie => movie.Title)
+        setMovies(movies)
+      })
+    }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+        movies.map(singleMovie => <h1>{singleMovie}</h1>)
+        }
       </header>
     </div>
   );
